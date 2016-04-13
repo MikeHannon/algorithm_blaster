eb5b261f6fedf61a7586985d801f9d72effdb57c
Mike Hannon <mjhannon@gmail.com>
2016-04-12 21:21:35 -0700

    updated falling and scoring, testing stuff

--- client/assets/js/object.js
+++ client/assets/js/object.js
@@ -1,14 +1,22 @@
+var ball = {
+  x : 50,
+  y : 100,
+  radius: 20,
+  arc_start: 0,
+  arc_end: 2*Math.PI,
+  id:"ball",
+  fall_rate: 3,
+}
+var score = 0;
+
+function update_score(){
+  $("#score").text(10+parseInt($("#score").text()));
+}
+
+
 $(document).ready(function(){
   console.log("working");
-  var ball = {
-    x : 50,
-    y : 100,
-    radius: 20,
-    arc_start: 0,
-    arc_end: 2*Math.PI,
-    id:"ball",
-    fall_rate: 0.5,
-  }
+
   $('#game_board').html();
   var myCanvas = '<canvas id="myCanvas" width="500" height="500" style="border:1px solid #000000;"></canvas>';
   $('#game_board').html(myCanvas);
@@ -18,17 +26,27 @@ $(document).ready(function(){
        ctx.fillStyle = "black";
        ctx.fillRect(700, 100, 100, 100);
 
-       function draw(target) {
+       function draw() {
            ctx.beginPath();
-           ctx.arc(target.x, target.y,target.radius, target.arc_start, target.arc_end);
+           ctx.arc(ball.x, ball.y, 20, 0, 2 * Math.PI);
            ctx.fillStyle = 'rgba(250,0,0,0.4)';
            ctx.fill();
-           y += target.fall_rate;
-          //  ctx.fillStyle = "rgba(34,45,23,0.4)";
-          //  ctx.fillRect(0, 0, c.width, c.height);
+           ball.y += ball.fall_rate;
+           ctx.fillStyle = "rgba(34,45,23,0.4)";
+           ctx.fillRect(0, 0, c.width, c.height);
            requestAnimationFrame(draw);
+           if (ball.y > 500){
+             ball.y = 500;
+             score += 10;
+             update_score();
+             ball.fall_rate = 0;
+           }
            //ctx.clearRect(0,0,can.width,can.height);
        }
-       draw(ball);
+       draw();
 
+$(document).click(function(){
+  ball.fall_rate = 0;
+});
+// dictionary of algorithms
 })
